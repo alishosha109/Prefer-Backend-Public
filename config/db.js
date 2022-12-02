23c24b87850eee0348aa63a310e612702864bf72
Ali Shosha <75977417+alishosha109@users.noreply.github.com>
2022-12-02 19:43:01 +0200

    Delete config directory

--- config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    url: "mongodb+srv://PreferAdmin:6901241Prefer@prefer.ublkcpk.mongodb.net/?retryWrites=true&w=majority",
-    database: "prefer",
-    imgBucket: "photos",
-  }
